b6cbac5fac67f47597a4e78ef45bd82546547c2a
Julian Andres Alarcon <julian.alarcon@globant.com>
2022-03-31 13:29:38 -0500

    add typescript to project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Header from './shared/components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
