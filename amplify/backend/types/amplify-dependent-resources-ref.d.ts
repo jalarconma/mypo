a488a8ff45b4d9463ecf9cf6e5c23d5fac7e27b2
Julian Andres Alarcon <julian.alarcon@globant.com>
2022-04-04 17:23:55 -0500

    add basic amplify to dev environment

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "mypo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "populateSymbols": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
