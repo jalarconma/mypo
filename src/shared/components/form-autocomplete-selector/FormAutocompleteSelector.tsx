fcb8cf7e218a80fc7ac1349a31c1794531bf8cb5
Julian Alarcon <juaalarconma@gmail.com>
2022-08-27 16:40:42 -0500

    add virtualized list to symbols

--- src/shared/components/form-autocomplete-selector/FormAutocompleteSelector.tsx
+++ src/shared/components/form-autocomplete-selector/FormAutocompleteSelector.tsx
@@ -2,10 +2,140 @@ import React from "react";
 
 import { Controller } from "react-hook-form";
 
-import Autocomplete from "@mui/material/Autocomplete";
+import Autocomplete, { autocompleteClasses } from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 
-const FormAutocompleteSelector = ({ control, name, options, label = '', rules = {}, multiple = false}) => {
+import useMediaQuery from '@mui/material/useMediaQuery';
+import ListSubheader from '@mui/material/ListSubheader';
+import Popper from '@mui/material/Popper';
+import { useTheme, styled } from '@mui/material/styles';
+import { VariableSizeList, ListChildComponentProps } from 'react-window';
+import Typography from '@mui/material/Typography';
+import { FormSelectorOption } from "../../../core/models/form-selector-option.interface";
+
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props: ListChildComponentProps) {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  const inlineStyle = {
+    ...style,
+    top: (style.top as number) + LISTBOX_PADDING,
+  };
+
+  if (dataSet.hasOwnProperty('group')) {
+    return (
+      <ListSubheader key={dataSet.key} component="div" style={inlineStyle}>
+        {dataSet.group}
+      </ListSubheader>
+    );
+  }
+
+  return (
+    <Typography component="li" {...dataSet[0]} noWrap style={inlineStyle}>
+      {dataSet[1].label}
+    </Typography>
+  );
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data: any) {
+  const ref = React.useRef<VariableSizeList>(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLElement>
+>(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData: React.ReactChild[] = [];
+  (children as React.ReactChild[]).forEach(
+    (item: React.ReactChild & { children?: React.ReactChild[] }) => {
+      itemData.push(item);
+      itemData.push(...(item.children || []));
+    },
+  );
+
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), {
+    noSsr: true,
+  });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = (child: React.ReactChild) => {
+    if (child.hasOwnProperty('group')) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+
+const StyledPopper = styled(Popper)({
+  [`& .${autocompleteClasses.listbox}`]: {
+    boxSizing: 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
+
+interface Props {
+  control: any;
+  name: string;
+  options: FormSelectorOption[];
+  label?: string;
+  rules?: any;
+  multiple?: boolean;
+}
+
+const FormAutocompleteSelector = ({ control, name, options, label = '', rules = {}, multiple = false}: Props) => {
   return (
     <Controller
       name={name}
@@ -17,12 +147,18 @@ const FormAutocompleteSelector = ({ control, name, options, label = '', rules = 
           disablePortal
           options={options}
           value={value}
+          disableListWrap
+          PopperComponent={StyledPopper}
+          ListboxComponent={ListboxComponent}
           onChange={(event, data) => {
             onChange(data);
             return data;
           }}
           isOptionEqualToValue={(option, value) => option.id === value.id}
           renderInput={(params) => <TextField error={!!error} helperText={error?.message} {...params} label={label} />}
+          renderOption={(props, option: FormSelectorOption) => [props, option] as React.ReactNode}
+          // TODO: Post React 18 update - validate this conversion, look like a hidden bug
+          renderGroup={(params) => params as unknown as React.ReactNode}
         />
       )}
     />
